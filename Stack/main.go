f4722e47682ec011410abb364ac708ba760f62cd
iwtrgw <iwtrgw@gmail.com>
2022-03-28 10:52:12 +0800

    链栈的实现：Test

--- Stack/main.go
+++ Stack/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Node 定义链表节点
 type Node struct {
 	Value int
@@ -26,6 +30,70 @@ func Push(v int) bool {
 	return true
 }
 
-func main() {
+// Pop 出栈
+func Pop(n *Node) (int, bool) {
+	// 空栈
+	if size == 0 {
+		return 0, false
+	}
 
+	// 一个节点
+	if size == 1 {
+		size = 0
+		stack = nil
+		return n.Value, true
+	}
+	// 将栈的头节点指针指向下一个节点，并返回之前的头节点数据
+	stack = stack.Next
+	size--
+	return n.Value, true
+}
+
+// 遍历
+func traverse(n *Node) {
+	if size == 0 {
+		fmt.Println("空栈")
+		return
+	}
+	for n != nil {
+		fmt.Printf("%d -> ", n.Value)
+		n = n.Next
+	}
+	fmt.Println()
+}
+func main() {
+	stack = nil
+	// 读取空栈
+	v, b := Pop(stack)
+	if b {
+		fmt.Print(v, " ")
+	} else {
+		fmt.Println("Pop() 失败!")
+	}
+
+	// 压栈
+	Push(100)
+	// 遍历栈
+	traverse(stack)
+	Push(200)
+	traverse(stack)
+
+	// 批量进栈
+	for i := 0; i < 10; i++ {
+		Push(i)
+	}
+
+	// 批量出栈
+	for i := 0; i < 15; i++ {
+		v, b := Pop(stack)
+		if b {
+			fmt.Print(v, " ")
+		} else {
+			// 如果已经是空栈，则退出循环
+			break
+		}
+	}
+	fmt.Println()
+	// 再次遍历栈
+	traverse(stack)
 }
